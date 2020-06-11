c30a50ac04a4808adb6ce6e491c37657131e172d
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-11 23:45:07 +0530

    Add LeetCode #0003 in Python.

---
+++ Python/0003-longest-substring-without-repeating-chars.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        dicts = {}
+        maxlength = start = 0
+        for i,value in enumerate(s):
+            if value in dicts:
+                sums = dicts[value] + 1
+                if sums > start:
+                    start = sums
+            num = i - start + 1
+            if num > maxlength:
+                maxlength = num
+            dicts[value] = i
+        return maxlength
+
