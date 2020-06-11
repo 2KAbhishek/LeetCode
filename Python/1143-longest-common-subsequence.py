85bd0549e4acc0d4a7d1e82880427e983a89271d
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-11 23:45:05 +0530

    Add LeetCode #1143 in Python.

---
+++ Python/1143-longest-common-subsequence.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        """
+        :type text1: str
+        :type text2: str
+        :rtype: int
+        """
+        n,m=len(text1),len(text2)
+        dp=[[0]*(m+1) for _ in range(n+1)]
+        for i in range(n):
+            for j in range(m):
+                if text1[i]==text2[j]:
+                    dp[i+1][j+1]=dp[i][j]+1
+                else:
+                    dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j])
+        return dp[-1][-1]
+
