5d504bace889471376fe9658627f6250fe17c6b0
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-11 23:45:05 +0530

    Add LeetCode #1008 in Python.

---
+++ Python/1008-bst-from-preorder.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def bstFromPreorder(self, preorder):
+        """
+        :type preorder: List[int]
+        :rtype: TreeNode
+        """
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        i = 1
+        while i<len(preorder) and  preorder[i] < root.val:
+            i+=1
+        root.left = self.bstFromPreorder(preorder[1:i])
+        root.right = self.bstFromPreorder(preorder[i:])
+        return root
+
