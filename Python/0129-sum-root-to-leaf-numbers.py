387542d7aa763fe8263dc70b12224436b91e97f4
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-06-28 00:35:41 +0530

    Add LeetCode #0129 in Python.

---
+++ Python/0129-sum-root-to-leaf-numbers.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        stack, res = [(root, root.val)], 0
+
+        while stack:
+            node, value = stack.pop()
+            if node:
+                if not node.left and not node.right:
+                    res += value
+                if node.right:
+                    stack.append((node.right, value*10+node.right.val))
+                if node.left:
+                    stack.append((node.left, value*10+node.left.val))
+
+        return res
