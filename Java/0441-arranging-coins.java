046b20d17f837e19d692aab74e2062347c2453fe
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-07-02 12:58:04 +0530

    Add LeetCode #0441 in Java.

---
+++ Java/0441-arranging-coins.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int arrangeCoins(int n) {
+        long left = 0, right = n, k, curr;
+
+        while (left <= right){
+            k =  left + (right - left) / 2;
+            curr = k * (k + 1) / 2;
+
+            if (curr == n) return (int)k;
+
+            if (curr > n)
+                right = k - 1;
+            else
+                left = k + 1;
+        }
+
+        return (int)right;
+    }
+}
+
