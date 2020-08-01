4f9cb314f47b93d90c0f0d8b6d0e6feaf817da80
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-01 13:15:51 +0530

    Update LeetCode #0070 in Python.

--- Python/0070-climbing-stairs.py
+++ Python/0070-climbing-stairs.py
@@ -1,7 +1,17 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        a = b = 1
-        for _ in range(n):
-            a, b = b, a + b
-        return a
+        # a = b = 1
+        # for _ in range(n):
+        #     a, b = b, a + b
+        # return a
+
+        memo = {}
+        memo[0] = 1
+        memo[1] = 1
+
+        for k in range(2,n+1):
+            f = memo[k-1] + memo[k-2]
+            memo[k] = f
+
+        return memo[n]
 
