6afd9813f5af863668265264df3a5d83279c4c57
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-08-05 09:40:30 +0530

    Update LeetCode #0231 in Python.

--- Python/0231-power-of-two.py
+++ Python/0231-power-of-two.py
@@ -1,7 +1,7 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        i = 1
-        while i < n:
-            i *= 2
-        return i == n
-
+        # i = 1
+        # while i < n:
+        #     i *= 2
+        # return i == n
+        return n>0 and (n & n-1) == 0
