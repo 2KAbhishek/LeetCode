ce3b3704a32b31d7539c7e0015ea816d3bd8d53e
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-02-18 10:20:56 +0530

    Add Leetcode #0977 in Python

---
+++ Python/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i] = nums[i] * nums[i]
+        nums.sort()
+        return nums
+        #return sorted(list(map(lambda x: x * x, nums)))
+
