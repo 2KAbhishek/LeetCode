2a01b1b175155c161f150af2e54ae837c4038c26
Abhishek Keshri <keshri.abhishek63@gmail.com>
2022-05-03 17:53:23 +0530

    Time: 124 ms (48.72%), Space: 211.9 MB (7.69%) - LeetHub

---
+++ 1089-duplicate-zeros/1089-duplicate-zeros.rb
@@ -0,0 +1,17 @@
+# @param {Integer[]} arr
+# @return {Void} Do not return anything, modify arr in-place instead.
+def duplicate_zeros(arr)
+    length = arr.length
+    newArr = []
+    arr.each do |i|
+        unless newArr.length >= arr.length
+            if i == 0 and (newArr.length + 2) <= arr.length
+                newArr.insert(newArr.length, i)
+                newArr.insert(newArr.length, i)
+            else
+                newArr.insert(newArr.length, i)
+            end
+        end
+    end
+    (0...arr.size).each{|i| arr[i] = newArr[i]}
+end
